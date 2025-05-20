fd2ae900db274cd0d3e99cf6fefc950569fbaca6
samnjoro <samnjorokibandi@gmail.com>
2025-05-20 16:49:46 +0300

    connecting the auth for register, login

--- server/controller/auth.js
+++ server/controller/auth.js
@@ -1,36 +1,52 @@
 const auth = require('../models/auth');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
 const register =async (req, res) => {
-    const {username, email, password} = req.body;
+    try{
+      const {username, email, password} = req.body;
 
-    const existingUser = await auth.findOne({ email })
-    if(existingUser){
+      const existingUser = await auth.findOne({ email })
+      if(existingUser){
         return res.status(400).json({ message: "User exists"});
-    }
+      }
 
-    const hashedPassword = await bcrypt.hash(password, 12)
+      const hashedPassword = await bcrypt.hash(password, 12)
 
-    const User = new auth({
+      const User = new auth({
         username,
         email,
         password: hashedPassword
-    })
-    await User.save
-    res.status(201).json({ message: 'User registered successfully' });
+       })
+      await User.save();
+      res.status(201).json({ message: 'User registered successfully' });
+    }catch(error){
+        console.log("Failed to register user", error);
+        res.status(500).json({message: "Server Error"});
+   }
 }
 
 const login = async(req, res) => {
-    const { email, password} = req.body;
+    try{
+      const { email, password} = req.body;
 
-    const user = await auth.findOne({email});
-    if(!user) return register.status(400).json({message: "email doesn't exist"})
+      const user = await auth.findOne({email});
+      if(!user) return register.status(400).json({message: "email doesn't exist"})
 
-    const isMatch = await bcrypt.compare(password, auth.password);
-    if(!isMatch) return res.status(400).json({message : "Incorrect Password"})
+      const isMatch = await bcrypt.compare(password, auth.password);
+      if(!isMatch) return res.status(400).json({message : "Incorrect Password"})
 
-    const token = jwt.sign({id: user._id}, process.env.JWT_SECRET, {expiresIn: '2h'});
+      const token = jwt.sign({id: user._id}, process.env.JWT_SECRET, {expiresIn: '2h'});
 
-    res.json({ message: 'Login successful', token });
+      res.json({ message: 'Login successful', token });
+    }catch(error){
+        res.status(500).json({message: "Error loging in"});
+        console.log("server error loging in", error);
+    }
+}
 
+module.exports = {
+    register,
+    login
 }
--- server/index.js
+++ server/index.js
@@ -1,7 +1,14 @@
 const http = require('http');
-const express = require('expresss');
+const express = require('express');
 
 const app = express();
+
+const auth = require("./routes/auth");
+
+
+app.use('/api/auth', auth);
+
+const port = process.env.PORT;
 
 app.listen(port, () =>{
     console.log(`server running on port ${port}`);
