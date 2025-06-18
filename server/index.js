6db577839ddc6498baef9edec8101f88c186b481
samnjoro <samnjorokibandi@gmail.com>
2025-06-18 11:59:46 +0300

    backend database connect

--- server/index.js
+++ server/index.js
@@ -1,10 +1,21 @@
 const http = require('http');
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
 const auth = require("./routes/auth");
 
+
+const DB = () =>{
+    try{
+       connect.mongoose(process.env.MONGO_URL)
+       console.log("COnnected to mongo successfull", databaseConnect);
+    }catch(error){
+        console.error("Cant connect to database", error);
+    }
+}
+DB()
 
 app.use('/api/auth', auth);
 
