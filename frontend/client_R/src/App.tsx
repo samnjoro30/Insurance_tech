127d8805ef9dedd14bab25595efcd6a9f7d8da6e
samnjoro <samnjorokibandi@gmail.com>
2025-07-07 21:08:54 +0300

    app.tsx update for deplyment

--- frontend/client_R/src/App.tsx
+++ frontend/client_R/src/App.tsx
@@ -1,11 +1,9 @@
-import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Register from './components/register';
 import Home from './pages/home';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <Router>
