f214e94f78a50c24b3b635ee033f382351b90ed7
samnjoro <samnjorokibandi@gmail.com>
2025-07-17 18:20:52 +0300

    header setup

--- frontend/client_R/src/App.tsx
+++ frontend/client_R/src/App.tsx
@@ -1,7 +1,8 @@
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Register from './components/register';
 import Home from './pages/home';
-import './App.css'
+import DashUsers from './pages/dash';
+import './App.css';
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
           <Route path="/" element ={<Home />}/>
           <Route path="/login" element={<Register initialMode="login" />} />
           <Route path="/register" element={<Register initialMode="register" />} />
+          <Route path="/dashboard" element= {<DashUsers/>} />
         </Routes>
     </Router>
   )
--- frontend/client_R/src/pages/dash.tsx
+++ frontend/client_R/src/pages/dash.tsx
@@ -1,12 +1,18 @@
 
 import HeaderUser from '../components/headersub';
+import Header from '../components/header';
 
 export default function DashUsers () {
 
 
     return(
         <div>
-            < HeaderUser />
+            <header>
+                <Header />
+            </header>
+            <main>
+                <HeaderUser />
+            </main>
 
         </div>
     )
