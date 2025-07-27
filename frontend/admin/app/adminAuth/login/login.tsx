e35341281b25717f920ebc8b3e0d6720c04a746d
samnjoro <samnjorokibandi@gmail.com>
2025-07-27 15:22:02 +0300

    login page for admin

---
+++ frontend/admin/app/adminAuth/login/login.tsx
@@ -0,0 +1,83 @@
+"use client"
+
+import { useState } from 'react';
+import axios from 'axios';
+
+interface FormData {
+    email: string;
+    workId: string;
+    password: string;
+}
+
+export default function Adminlogin (){
+    const [loading, setLoading] = useState<boolean>(false);
+    const [formData, setFormData] = useState<FormData>({
+        email: '',
+        workId: '',
+        password: '',
+    })
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value} = e.target;
+        setFormData(prev => ({ ...prev, [name]: value}))
+    }
+
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+
+
+        try{
+            const res = await axios.post("admin/login", formData)
+            setFormData(res.data.message);
+        }catch(err){
+
+        }finally{
+
+        }
+    }
+
+    return(
+        <div>
+            <div> 
+                <form onSubmit={handleSubmit}>
+                    <div>
+                        <label>Email:</label>
+                        <input
+                          type=""
+                          name=""
+                          value=""
+                          onChange={handleChange}
+                          required
+                        />
+                    </div>
+                    <div>
+                        <label>WorkId</label>
+                        <input
+                          type=""
+                          name=""
+                          value=""
+                          onChange={handleChange}
+                          required
+                        />
+                    </div>
+                    <div>
+                        <label>Password</label>
+                        <input
+                          type=""
+                          name=""
+                          value=""
+                          onChange={handleChange}
+                          required
+                        />
+                    </div>
+                    <button
+                       type="submit"
+                    >
+                        {loading ? 'loging..' : 'login'}
+                    </button>
+                </form>
+            </div>
+
+        </div>
+    )
+
+}
