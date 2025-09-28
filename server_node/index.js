b582d8b447095bad8b6d2eeacccba007dc42a8d9
samnjoro <samnjorokibandi@gmail.com>
2025-09-28 23:42:46 +0300

    backend server url

--- server_node/index.js
+++ server_node/index.js
@@ -9,7 +9,10 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: [ 
+        'http://localhost:5173',
+        "https://insurance-tech-xi.vercel.app",
+    ]
     credentials: true
  })) 
 
